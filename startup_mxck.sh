a32f0e55d7f5765007b7011a1161de41aa7912fd
william-mx <109865684+william-mx@users.noreply.github.com>
2024-03-15 12:59:32 +0100

    change hotspot name

--- startup_mxck.sh
+++ startup_mxck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# set up a hotspot with the name 'mxck' and the password 'mxck1234'
-sudo nmcli dev wifi hotspot ifname wlan0 ssid mxck password mxck1234
+# set up a hotspot with the name 'mxck0000' and the password 'mxck0000'
+sudo nmcli dev wifi hotspot ifname wlan0 ssid mxck0000 password mxck0000
 
 # run docker in new terminal
 sleep 5
